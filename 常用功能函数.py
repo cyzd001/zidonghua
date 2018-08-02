eebb775b4ebe5bc0744a672c9fb185df6d875ee0
cyzd001 <41434590+cyzd001@users.noreply.github.com>
2018-08-02 17:30:48 +0800

    Add files via upload

--- 常用功能函数.py
+++ 常用功能函数.py
@@ -6,27 +6,8 @@ import HTMLTestRunner
 import time
 import os
 import sys
-
-'''三个数相加和值，几种组合'''
-def hezhi(num):
-    import random
-    n = 1
-    result = {}
-    while n <= 1000:
-        a = random.randint(0, 9)
-        b = random.randint(0, 9)
-        c = random.randint(0, 9)
-        if a + b + c == num:
-            result[n] = (a, b, c)
-            n = n + 1
-    print(result)
-    print(n)
-    func = lambda z: dict([(x, y) for y, x in z.items()])
-    print((func(func(result))))
-    num = [a for a in func(func(result))]
-    print(len(num))
-hezhi(21)
 ''''''
+"""随机数"""
 def suiji(num):
     """生成num个随机数功能函数"""
     result =[]
@@ -76,6 +57,83 @@ def xuhuan(num): #num代表循环次数
             sys.exit(0)
 
 ''''''
+"""组合数"""
+def zuhe(m,n):
+    c = 1
+    b = 1
+    d = 1
+    num2 = 1
+    num1 = 1
+    num3 = 1
+    while c <= m:
+        if c <= m:
+            num1 = num1 * c
+            c = c +1
+    while b <= n:
+        if b <= n:
+            num2 = num2 * b
+            b = b + 1
+    while d <= m-n:
+        if d <= m-n:
+            num3 = num3 * d
+            d = d + 1
+    print(num1,num2,num3)
+    num = int(num1 /(num2*num3))
+    print(num)
+    return num
+zuhe(11,9)
+"""排列数"""
+def pailei(m,n):
+    c = 1
+    b = 1
+    num2 = 1
+    num1 = 1
+    while c <= m-n:
+        if c <= m-n:
+            num1 = num1*c
+            c = c + 1
+    while b <= m:
+        if b <= m:
+            num2 = num2*b
+            b = b + 1
+    print(num2,num1)
+    num = int(num2/num1)
+    print(num)
+    return num
+pailei(9,2)
+
+"""读取和写入xls数据"""
+'''往xls表格写入数据'''
+import xlwt
+import xlrd
+wb = xlwt.Workbook()
+ws = wb.add_sheet('excel.xls')
+lb = [('username','password','money'),('sb001','qwaszx',3),('sb002','qwaszx1',1),('sb003','qwaszx12',2),('tt001','qwaszx12',1)]
+c = 1
+m = 0
+for i in lb:
+        num1 = i[0]
+        num2 = i[1]
+        num3 = i[2]
+        if c > 0:
+            ws.write(m, 0, num1)
+            ws.write(m, 1, num2)
+            ws.write(m, 2, num3)
+            m = m + 1
+wb.save('excel.xls')
+'''读取excel表格数据'''
+excel = xlrd.open_workbook('excel.xls')
+nums = excel.sheet_by_index(0)
+for n in range(1,5):
+    for m in range(0,1):
+        data = nums.cell(n, m).value
+        da = nums.cell(n,m+1).value
+        rmb = int(nums.cell(n,m+2).value)
+        print (data,da,rmb)
+        # print(data,end="")
+        # print(da,end="")
+        # print(rmb)
+
 """带框架跑自动化"""
 class mytest(unittest.TestCase):
     def mukai001(self):
